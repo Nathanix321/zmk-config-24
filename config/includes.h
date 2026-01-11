1c01bad4d0c5c92aaf8699a475a28b9dd08c9f03
Nathanix321 <letourneau.nathaniel@hotmail.com>
2026-01-11 01:30:31 -0500

    maybe this'll do it

--- config/includes.h
+++ config/includes.h
@@ -1,8 +1,13 @@
-#include <behaviors.dtsi>
+// #include <behaviors.dtsi>
 
-#include <dt-bindings/zmk/bt.h>
-#include <dt-bindings/zmk/keys.h>
-#include <dt-bindings/zmk/outputs.h>
+// #include <dt-bindings/zmk/bt.h>
+// #include <dt-bindings/zmk/keys.h>
+// #include <dt-bindings/zmk/outputs.h>
+
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/mouse.h>
 
 #include "zmk-helpers/helper.h"
 
