436af1821af9d6c914db7bc0d1d8a516aa64dabc
Nathanix321 <letourneau.nathaniel@hotmail.com>
2026-01-10 23:29:50 -0500

    rearrange includes to fix error

--- config/includes.h
+++ config/includes.h
@@ -1,10 +1,10 @@
-#include "config.dtsi"
+#include <behaviors.dtsi>
 
+#include <dt-bindings/zmk/bt.h>
+#include <dt-bindings/zmk/keys.h>
+#include <dt-bindings/zmk/outputs.h>
+
+#include "zmk-helpers/helper.h"
 #include "zmk-helpers/key-labels/36.h"
 
-#include <behaviors.dtsi>
-#include <dt-bindings/zmk/keys.h>
-#include <dt-bindings/zmk/bt.h>
-#include <dt-bindings/zmk/outputs.h>
-#include "zmk-helpers/helper.h"
-
+#include "config.dtsi"
