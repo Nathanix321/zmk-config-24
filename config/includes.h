d61cb0c127296e297abdbfa15f2fa6a4b729e15b
Nathanix321 <letourneau.nathaniel@hotmail.com>
2026-01-10 23:20:25 -0500

    strip down keymap, move other stuff in config and includes.h

--- config/includes.h
+++ config/includes.h
@@ -1,1 +1,10 @@
 #include "config.dtsi"
+
+#include "zmk-helpers/key-labels/36.h"
+
+#include <behaviors.dtsi>
+#include <dt-bindings/zmk/keys.h>
+#include <dt-bindings/zmk/bt.h>
+#include <dt-bindings/zmk/outputs.h>
+#include "zmk-helpers/helper.h"
+
