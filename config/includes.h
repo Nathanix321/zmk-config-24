513d95ce9a3dadfc8df7cdbfbf69c987e2146140
Nathanix321 <letourneau.nathaniel@hotmail.com>
2026-01-17 23:33:15 -0500

    Add bootloader support, remove layers

--- config/includes.h
+++ config/includes.h
@@ -8,6 +8,7 @@
 
 #include "features/accents.dtsi"
 #include "features/adaptive.dtsi"
+#include "features/bootloader.dtsi"
 #include "features/homerow.dtsi"
 #include "features/macros.dtsi"
 #include "features/morphs.dtsi"
