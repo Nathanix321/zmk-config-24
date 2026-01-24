313d194386c7544a08b09033c5c22ad1ce5bb533
Nathanix321 <letourneau.nathaniel@hotmail.com>
2026-01-24 17:44:25 -0500

    macros should be included before, since we use them everywhere else

--- config/includes.h
+++ config/includes.h
@@ -6,13 +6,6 @@
 #include "helpers.h"
 #include "config.dtsi"
 
-#include "features/accents.dtsi"
-#include "features/adaptive.dtsi"
-#include "features/bootloader.dtsi"
-#include "features/combos.dtsi"
-#include "features/homerow.dtsi"
-#include "features/morphs.dtsi"
-#include "features/tapholds.dtsi"
 
 #include "features/macros/arcane_macros.dtsi"
 #include "features/macros/currency_macros.dtsi"
@@ -21,3 +14,11 @@
 #include "features/macros/symbol_macros.dtsi"
 #include "features/macros/text_macros.dtsi"
 #include "features/macros/url_macros.dtsi"
+
+#include "features/accents.dtsi"
+#include "features/adaptive.dtsi"
+#include "features/bootloader.dtsi"
+#include "features/combos.dtsi"
+#include "features/homerow.dtsi"
+#include "features/morphs.dtsi"
+#include "features/tapholds.dtsi"
